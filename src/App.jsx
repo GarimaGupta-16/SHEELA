d6ead4ebe288de53419794f2936ded6c416ec006
Gurleen2506 <gurleen0299.becse24@chitkara.edu.in>
2025-11-04 15:37:11 +0530

    adityaaaa

--- src/App.jsx
+++ src/App.jsx
@@ -19,7 +19,7 @@ function App() {
       <h1>Vite + React</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
-          number of shela's is {count}
+          number of sheeeeeeeela's is {count}
         </button>
         <p>
           Edit <code>src/App.jsx</code> save to test HMR
